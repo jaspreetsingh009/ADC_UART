b873d1c7c39a42db3e23b7667c717ff534be2388
Jaspreet Singh <jaspreetsingh009@gmail.com>
2017-05-23 19:34:47 +0530

    Update ADC.vhd

--- ADC.vhd
+++ ADC.vhd
@@ -4,7 +4,6 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ADC is
 port (	
-	
 	--- ADC Pins ---
 	Clk      :  IN  STD_LOGIC;
 	oDIN     :  OUT STD_LOGIC;
@@ -32,27 +31,27 @@ end entity ADC;
 architecture ADC_ARC of ADC is
 
 type   ADC_STATE is ( ADC_SEL, TRIGGER, ACQUIRE, STORE );
-signal NEXT_STATE	  : ADC_STATE;
-signal oCLK	   	  : STD_LOGIC := '0';
+signal NEXT_STATE   : ADC_STATE;
+signal oCLK	    : STD_LOGIC := '0';
 signal CS_BUF       : STD_LOGIC := '0';
 signal TEST_OUT_BUF : STD_LOGIC := '0';
 
 begin
 
 
-oCS_n 	 <=  CS_BUF;
+oCS_n 	  <=  CS_BUF;
 TEST_OUT  <=  TEST_OUT_BUF;
 
 
 --- SCLK Selection ---
 
 with NEXT_STATE select
-   oSCLK 	<=  not oCLK  when ACQUIRE,
-			    '1'       	  when others;	
+   oSCLK    <=  not oCLK  when ACQUIRE,
+		'1'       when others;	
 
 
 ---------------------------------------------------------
---- 			 2.5MHz Clock for ADC 				  ---
+--- 	         2.5MHz Clock for ADC 		      ---
 ---------------------------------------------------------
 				 
 CLK_DELAY : process(CLK)
@@ -76,7 +75,7 @@ end process CLK_DELAY;
 				 
 
 ---------------------------------------------------------				 
---- 			    ADC State Machine 				  ---
+--- 	          ADC State Machine 	              ---
 ---------------------------------------------------------
 				 
 ADC_FSM : process( oCLK )
@@ -84,7 +83,7 @@ ADC_FSM : process( oCLK )
 	variable  DATA_COUNT   : INTEGER RANGE 0 TO 15;
 	variable  ADC_CH       : INTEGER RANGE 0 TO 7;
 	variable  ADC_DATA_BUF : INTEGER RANGE 0 TO 4095;
-	variable  iCH 		     : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	variable  iCH 	       : STD_LOGIC_VECTOR(2 DOWNTO 0);
 	variable  ADC_DATA     : STD_LOGIC_VECTOR(11 DOWNTO 0);
 	
 begin
@@ -97,16 +96,16 @@ begin
 			--- ADC Channel Selection ---
 		
 			when ADC_SEL =>  
-				
-					iCH := STD_LOGIC_VECTOR(TO_UNSIGNED( ADC_CH, 3 ));
 
-					if(ADC_CH = 7) then
-						ADC_CH := 0;  TEST_OUT_BUF <= not TEST_OUT_BUF;
-					else 
-						ADC_CH := ADC_CH + 1;
-					end if;
-					
-					NEXT_STATE <= TRIGGER;
+				iCH := STD_LOGIC_VECTOR(TO_UNSIGNED( ADC_CH, 3 ));
+
+				if(ADC_CH = 7) then
+					ADC_CH := 0;  TEST_OUT_BUF <= not TEST_OUT_BUF;
+				else 
+					ADC_CH := ADC_CH + 1;
+				end if;
+
+				NEXT_STATE <= TRIGGER;
 				
 		
 			--- Trigger ADC Conversion ---
@@ -128,10 +127,10 @@ begin
 		
 				case (DATA_COUNT) is 
 					
-					when 1   	=>  	oDIN		 	 <=  iCH(2);
-					when 2   	=>  	oDIN  		 <=  iCH(1);
-					when 3   	=>  	oDIN  		 <=  iCH(0);  
-					when 4		=>		ADC_DATA(11) :=  iDOUT;
+					when 1   	=>  	oDIN         <=  iCH(2);
+					when 2   	=>  	oDIN  	     <=  iCH(1);
+					when 3   	=>  	oDIN  	     <=  iCH(0);  
+					when 4		=>	ADC_DATA(11) :=  iDOUT;
 					when 5   	=>  	ADC_DATA(10) :=  iDOUT;
 					when 6   	=>  	ADC_DATA(9)  :=  iDOUT;
 					when 7   	=>  	ADC_DATA(8)  :=  iDOUT;
@@ -166,14 +165,14 @@ begin
 				
 				case (ADC_CH) is
 					
-					when 0  		=>  	ADC_CH6 <= ADC_DATA_BUF;
-					when 1  		=>  	ADC_CH7 <= ADC_DATA_BUF;
+					when 0  	=>  	ADC_CH6 <= ADC_DATA_BUF;
+					when 1  	=>  	ADC_CH7 <= ADC_DATA_BUF;
 					when 2 		=>  	ADC_CH0 <= ADC_DATA_BUF;
-					when 3  		=>  	ADC_CH1 <= ADC_DATA_BUF;
-					when 4  		=>  	ADC_CH2 <= ADC_DATA_BUF;
-					when 5  		=>  	ADC_CH3 <= ADC_DATA_BUF;
-					when 6  		=>  	ADC_CH4 <= ADC_DATA_BUF;
-					when 7  		=>  	ADC_CH5 <= ADC_DATA_BUF;
+					when 3  	=>  	ADC_CH1 <= ADC_DATA_BUF;
+					when 4  	=>  	ADC_CH2 <= ADC_DATA_BUF;
+					when 5  	=>  	ADC_CH3 <= ADC_DATA_BUF;
+					when 6  	=>  	ADC_CH4 <= ADC_DATA_BUF;
+					when 7  	=>  	ADC_CH5 <= ADC_DATA_BUF;
 					when others =>
 					
 				end case;
